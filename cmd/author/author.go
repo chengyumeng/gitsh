e5b56dcf33fd06b4e47054a65100becf895b3439
chengyumeng <792400644@qq.com>
2018-07-19 21:08:50 +0800

    提交更正

--- cmd/author/author.go
+++ cmd/author/author.go
@@ -7,31 +7,33 @@ import (
 )
 
 type Option struct {
-	Author string
 	Email string
-	NewAuthor string
+	NewName string
 	NewEmail string
 }
 
 var (
 	option Option
 	sh string = `git filter-branch -f --commit-filter '
-      if [ "$GIT_AUTHOR_EMAIL" = "%s"];
+      if [ "$GIT_AUTHOR_EMAIL" = "%s" ];
       then
 		GIT_AUTHOR_NAME="%s";
 		GIT_AUTHOR_EMAIL="%s";
-		GIT_COMMITTER_NAME="%s";
-		GIT_COMMITTER_EMAIL="%s";
+      fi
+      if [ "$GIT_COMMITTER_EMAIL" = "%s" ]
+      then
+        GIT_COMMITTER_NAME="%s"
+        GIT_COMMITTER_EMAIL="%s"
+      fi
         git commit-tree "$@";
-      else
-		git commit-tree "$@";
-      fi'  --tag-name-filter cat -- --branches --tags`
+      '  --tag-name-filter cat -- --branches --tags`
 	AuthorCmd = &cobra.Command{
 		Use:     "author",
 		Aliases: []string{"v"},
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(fmt.Sprintf(sh,option.Email,option.NewAuthor,option.NewEmail,option.NewAuthor,option.NewEmail))
-			c := exec.Command(fmt.Sprintf(sh,option.Email,option.NewAuthor,option.NewEmail,option.NewAuthor,option.NewEmail))
+			ex := fmt.Sprintf(sh,option.Email,option.NewName,option.NewEmail,option.Email,option.NewName,option.NewEmail)
+			c := exec.Command(ex)
+			fmt.Println(ex)
 			c.Output()
 		},
 	}
@@ -40,9 +42,8 @@ var (
 func init() {
 	flags := AuthorCmd.Flags()
 
-	flags.StringVarP(&option.Author, "author", "a", "", "原 author")
 	flags.StringVarP(&option.Email, "email", "e", "", "原 email")
-	flags.StringVarP(&option.NewAuthor, "new-author", "", "", "新 author")
+	flags.StringVarP(&option.NewName, "new-name", "", "", "新 name")
 	flags.StringVarP(&option.NewEmail, "new-email", "", "", "新 email")
 
 }
